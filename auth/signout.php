5cba35a49a2966b0a7f74364c21c53682a4a1f18
J-mar19 <balendezjacob123@gmail.com>
2026-09-09 15:36:54 +0800

    Updated index and RBAC funtions

---
+++ auth/signout.php
@@ -0,0 +1,22 @@
+<?php 
+require_once '../config/config.php';
+
+$_SESSION = [];
+
+session_destroy();
+
+header('Location:' . BASE_URL . '/index.php');
+exit;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>

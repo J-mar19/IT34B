61e5135fe1261beb29c5655795d8daf747844a58
J-mar19 <balendezjacob123@gmail.com>
2026-09-14 15:33:57 +0800

    added activity logs for login and logout session

--- auth/signout.php
+++ auth/signout.php
@@ -1,6 +1,10 @@
 <?php 
 require_once '../config/config.php';
 
+if(isset($_SESSION['user_id'])){
+    logActivity($pdo,$_SESSION['user_id'],$_SESSION['user_email'],'logout','success');
+}
+
 $_SESSION = [];
 
 session_destroy();
@@ -9,14 +13,3 @@ header('Location:' . BASE_URL . '/index.php');
 exit;
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
--- index.php
+++ index.php
@@ -13,15 +13,24 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $login = trim($_POST['login'] ?? '');
     $password =$_POST['password'] ?? '';
 
-    if(loginUser($pdo,$login,$password)){
-        echo 'Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php';
-        header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
-    exit;
+ $error = 'Invalid login credentials';
 
+    if ($login=== '' || $password === ''){
+        //Log Incomplete
+        logActivity($pdo,null,$login,'login','failed');
+    }else{
+        
+        if(loginUser($pdo,$login,$password)){
+            //Log complete login attempt
+            logActivity($pdo,$_SESSION['user_id'],$_SESSION['user_email'],'login','success');
+            header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
+
+        }
     }
 
-    $error = 'Invalid login credentials';
 }
+
+   
 
 ?>
 
